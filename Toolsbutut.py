3ad3bba510429dd872483c7979ce9b716679d4c2
xxoprt <86563093+xxoprt@users.noreply.github.com>
2021-08-01 10:00:49 +0700

    Add files via upload

---
+++ Toolsbutut.py
@@ -0,0 +1,160 @@
+#!/bin/python2
+#Code by MR.FAGHP BLACK-404/F
+#NO RECODE BNGST
+#modules
+import os
+from time import sleep
+
+#warna
+hijau = "\33[0;32m"
+#codingan
+os.system('clear')
+sleep(2)
+print hijau+' ______          __    __        __       __ '
+print hijau+'/_  __/__  ___  / /__ / /  __ __/ /___ __/ /_ '
+print hijau+' / / / _ \/ _ \/ (_-</ _ \/ // / __/ // / __/ '
+print hijau+'/_/  \___/\___/_/___/_.__/\_,_/\__/\_,_/\__/ '
+sleep(3)
+print hijau+('##############################################')
+print hijau+('Author:MR.FAGHP BLACK-404/F')
+print hijau+('##############################################')
+print hijau+('Toolsbutut menyediakan script hacking')
+print hijau+('##############################################')
+sleep(1)
+print hijau+('(1).permium')
+sleep(1)
+print hijau+('(2).ymbf')
+sleep(1)
+print hijau+('(3).simbf')
+sleep(1)
+print hijau+('(4).Sensei')
+sleep(1)
+print hijau+('(5).ddosWD')
+sleep(1)
+print hijau+('(6).LITEDDOS')
+sleep(1)
+print hijau+('(7).MCIDDOS')
+sleep(1)
+print hijau+('(8).ipcs')
+sleep(1)
+print hijau+('(9).Cam-Hackers')
+sleep(1)
+print hijau+('(10).T4MPILAN-V5')
+sleep(1)
+print hijau+('(11).dark_wallpaper')
+sleep(1)
+print hijau+('(12).onex')
+sleep(1)
+print hijau+('(13)Tool-X')
+sleep(1)
+print hijau+('(14)exit')
+sleep(1)
+pilih = input('\33[0;32m===> ')
+
+if pilih == 1:
+     os.system('clear')
+     os.system('figlet Tunggu')
+     sleep(1)
+     os.system('rm -rf premium')
+     os.system('git clone https://github.com/Dapunta/premium')
+     os.system('clear')
+     sleep(1)
+     os.system('figlet Selesai')
+
+if pilih == 2:
+     os.system('clear')
+     os.system('figlet Tunggu')
+     sleep(1)
+     os.system('git clone https://github.com/yayan-XD/ymbf')
+     os.system('clear')
+     sleep(1)
+     os.system('figlet Selesai')
+if pilih == 3:
+       os.system('clear')
+       os.system('figlet Tunggu')
+       sleep(1)
+       os.system('git clone https://github.com/Ibnustagnz/simbf')
+       os.system('clear')
+       sleep(1)
+       os.system('figlet Selesai')
+
+if pilih == 4:
+       os.system('clear')
+       os.system('figlet Tunggu')
+       sleep(1)
+       os.system('git clone https://github.com/NoobHackersCOM/Sensei')
+       os.system('clear')
+       sleep(1)
+       os.system('figlet Selesai')
+        
+if pilih == 5:
+       os.system('clear')
+       os.system('figlet Tunggu')
+       sleep(1)
+       os.system('git clone https://github.com/banghyuu/ddosWD')
+       os.system('clear')
+       sleep(1)
+       os.system('figlet Selesai')
+
+if pilih == 6:
+       os.system('clear')
+       os.system('figlet Tunggu')
+       sleep(1)
+       os.system('git clone https://github.com/4L13199/LITEDDOS')
+       sleep(1)
+       os.system('figlet Selesai')
+
+if pilih == 7:
+     os.system('clear')
+     os.system('figlet Tunggu')
+     sleep(1)
+     os.system('git clone https://github.com/MrTamfanX/MCIDDOS')
+     sleep(1)
+     os.system('figlet Selesai')
+
+if pilih == 8:
+      os.system('clear')
+      os.system('figlet Tunggu')
+      sleep(1)
+      os.system('git clone https://github.com/Kancotdiq/ipcs')
+      sleeep(1)
+      os.system('figlet Selesai')
+
+if pilih == 9:
+     os.system('clear')
+     os.system('figlet Tunggu')
+     sleep(1)
+     os.system('git clone https://github.com/AngelSecurityTeam/Cam-Hackers')
+     sleep(1)
+     os.system('figlet Selesai')
+
+if pilih == 10:
+       os.system('clear')
+       os.system('figelt Tunggu')
+       sleep(1)
+       os.system('git clone https://github.com/4NK3R-PRODUCT1ON/T4MPILAN-V5')
+       sleep(1)
+       os.system('figlet Selesai')
+if pilih == 11:
+       os.system('clear')
+       os.system('figlet Tunggu')
+       sleep(1)
+       os.system('git clone https://github.com/DARK-02/dark_wallpaper')
+       sleep(1)
+       os.system('figlet Selesai')
+
+if pilih == 12:
+      os.system('clear')
+      os.system('figlet Tunggu')
+      sleep(1)
+      os.system('git clone https://github.com/rajkumardusad/onex')
+      sleep(1)
+      os.system('figlet Selesai')
+
+elif pilih == 13:
+          os.system('clear')
+          os.system('figlet Tunggu')
+          sleep(1)
+          os.system('git clone https://github.com/rajkumardusad/Tool-X')
+          sleep(1)
+          os.system('figlet Selesai')
